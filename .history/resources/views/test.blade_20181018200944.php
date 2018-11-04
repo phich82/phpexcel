cce70d140938b3b9658c117151e1760e22118fb9
phich82 <phich82@gmail.com>
2018-11-04 23:19:55 +0700

    delete .history

--- .history/resources/views/test.blade_20181018200944.php
+++
@@ -1,7 +0,0 @@
-@if (!empty($patterns))
-    <ul>
-    @foreach ($patterns as $pattern)
-        <li><a href="{{ $pattern['file'] }}">{!! $pattern['title'] !!}</a></li>
-    @endforeach
-    </ul>
-@endif
--- .history/resources/views/test.blade_20181018201023.php
+++
@@ -1,7 +0,0 @@
-@if (!empty($patterns))
-    <ul style="border: 1px solid red;">
-    @foreach ($patterns as $pattern)
-        <li><a href="{{ $pattern['file'] }}">{!! $pattern['title'] !!}</a></li>
-    @endforeach
-    </ul>
-@endif
